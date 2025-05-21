c6ff11a3ae741d39b5bbe2503ffd932789641894
JuanFer1996 <juan_andres_fer@hotmail.com>
2025-05-21 11:03:55 -0300

    ejercicio2

--- Guia7.1/Ejercicio2/Program.cs
+++ Guia7.1/Ejercicio2/Program.cs
@@ -4,12 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Ejercicio2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            float masa, cantidadBudines, cantCajas,cantPaquetes,cantpaqueteSobrantes,masaSobrante;
+            Console.WriteLine("Ingrese cantidad de masa a producir en kg");
+            masa=int.Parse(Console.ReadLine());
+            masa=masa * 1000;
+            cantidadBudines = masa/55;
+            masaSobrante = masa % 55;
+            cantPaquetes = cantidadBudines / 12;
+            cantpaqueteSobrantes = cantidadBudines % 12;    
+            cantCajas = cantPaquetes / 20;
+
+            Console.WriteLine("Cantidad de budines: " + Math.Truncate(cantidadBudines));
+            Console.WriteLine("Masa sobrante: " + Math.Truncate(masaSobrante)+ " gr");
+            Console.WriteLine("Cantidad de paquetes: " + Math.Truncate(cantPaquetes));
+            Console.WriteLine("Cantidad de cajas completas: " + Math.Truncate(cantCajas));
+            Console.WriteLine("Cantidad de paquetes sobrantes: " + Math.Truncate(cantpaqueteSobrantes));
+
         }
     }
 }
